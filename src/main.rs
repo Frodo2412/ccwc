a69346780c800f6d95c5a810fc005bebae9e842a
bruno.lemus <bruno.lemus@globant.com>
2023-10-13 12:53:08 -0300

    feat: count bytes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let file_path = &args[2];
+
+    if query == "-c" {
+        println!("Counting bytes in file: {}", file_path);
+        let contents = fs::read_to_string(file_path)
+            .expect("File not found");
+
+        println!("{} {}", contents.len(), file_path);
+    }
 }
