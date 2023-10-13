d325ec972dfb005555387ae7f95c36337de2b776
bruno.lemus <bruno.lemus@globant.com>
2023-10-13 15:05:14 -0300

    feat: count words and lines

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,18 @@ fn main() {
     let query = &args[1];
     let file_path = &args[2];
 
-    if query == "-c" {
-        println!("Counting bytes in file: {}", file_path);
-        let contents = fs::read_to_string(file_path)
-            .expect("File not found");
+    let contents = fs::read_to_string(file_path)
+        .expect("File not found");
 
+    if query == "-c" {
         println!("{} {}", contents.len(), file_path);
+    } else if query == "-l" {
+        let lines: Vec<&str> = contents.split("\n").collect();
+        println!("{} {}", lines.len(), file_path);
+    } else if query == "-w" {
+        let words: Vec<&str> = contents.split_ascii_whitespace().collect();
+        println!("{} {}", words.len(), file_path);
+    } else {
+        println!("{} is not a supported flag", query);
     }
 }
